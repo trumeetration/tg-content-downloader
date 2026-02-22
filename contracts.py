4e39acf41f86418af014a5fc25cf2dcfb97fba5c
trum <trumeetration@gmail.com>
2026-02-23 00:19:59 +0500

    rename field to more convenient

--- contracts.py
+++ contracts.py
@@ -2,7 +2,7 @@ from typing import TypedDict, NotRequired
 
 class LinkResult(TypedDict):
     chat_id: int
-    result_video_url: str
+    result_content_url: str
     success: bool
     requested_url: str
     error: NotRequired[str]
