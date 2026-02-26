30548aa543a456b43556b721ef030d9077d4ad60
trum <trumeetration@gmail.com>
2026-02-26 13:50:39 +0500

    added thumbnail url to contract

--- contracts.py
+++ contracts.py
@@ -3,6 +3,7 @@ from typing import TypedDict, NotRequired
 class LinkResult(TypedDict):
     chat_id: int
     result_content_url: str
+    result_thumbnail_url: str
     success: bool
     requested_url: str
     error: NotRequired[str]
