c29cf8a6533924e4fb698014642f51648580a0d0
trum <trumeetration@gmail.com>
2026-03-04 22:47:25 +0500

    added table content_files

--- db/models.py
+++ db/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import List
+
+from sqlalchemy import String, ForeignKey, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
     pass
@@ -9,5 +11,13 @@ class CachedContent(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     url: Mapped[str] = mapped_column(String(500), unique=True, index=True)
+    files: Mapped[List["ContentFile"]] = relationship()
+
+
+class ContentFile(Base):
+    __tablename__ = "content_files"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content_id: Mapped[int] = mapped_column(ForeignKey("cached_content.id"))
     telegram_file_id: Mapped[str] = mapped_column(String(200))
-    name: Mapped[str] = mapped_column(String(200))
+    is_video: Mapped[bool] = mapped_column(Boolean())
